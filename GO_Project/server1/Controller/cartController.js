925442959b04242fbe1d32f6de8e40168933569f
user <user@user-HP-Compaq-Elite-8300-SFF>
2018-02-24 16:09:35 +0530

    by ANKA RAO

--- GO_Project/server1/Controller/cartController.js
+++ GO_Project/server1/Controller/cartController.js
@@ -12,3 +12,36 @@ exports.fetch = function(req,res){
     });
     console.log("Hey i reached here");
 }
+
+exports.data = function(req,res){
+    console.log("yahan to aa gya");
+    //res.write("hello");
+   
+   address.find(function(err,result){
+        if(err) throw err;
+        console.log(result);
+    });
+    console.log("Hey i reached here");
+}
+
+exports.insert = function(req,res){
+    console.log("yahan to aa gya");
+    //res.write("hello");
+   
+   address.find(function(err,result){
+        if(err) throw err;
+        console.log(result);
+    });
+    console.log("Hey i reached here");
+}
+
+exports.delete = function(req,res){
+    console.log("yahan to aa gya");
+    //res.write("hello");
+   
+   address.find(function(err,result){
+        if(err) throw err;
+        console.log(result);
+    });
+    console.log("Hey i reached here");
+}
