8772936dde6dd53156417555ed6b072a71cc9a28
user <user@user-Vostro-270s>
2018-02-26 19:43:35 +0530

    By: Nitish

--- GO_Project/server1/Controller/orderController.js
+++ GO_Project/server1/Controller/orderController.js
@@ -1,24 +1,28 @@
 var mongoose = require('mongoose');
 
 var order = mongoose.model("Order");
-exports.insert = function(req, res) {
+exports.insert = function (req, res) {
 	console.log("order insert me to aa gya");
 	//res.write("hello");
-	 var temp = new order(req.body)
-	 temp.save(req.body,function(err, result) {
-		if (err)
-			throw err;
-			res.send(true);
-		console.log(result);
-	});
-	//console.log("Hey i reached here");
+	for (var i = 0; i < req.body.length; i++) {
+		var temp = new order(req.body[i])
+		temp.save(req.body[i], function (err, result) {
+			if (err)
+				throw err;
+			console.log("hello")
+
+			console.log(result);
+		});
+		//console.log("Hey i reached here");
+	}
+	res.send(true);
 }
 
-exports.fetch = function(req, res) {
+exports.fetch = function (req, res) {
 	console.log("order fetch yahan to aa gya");
 	//res.write("hello");
 	myobj = req.body;
-	order.find(myobj,function(err, result) {
+	order.find(myobj, function (err, result) {
 		if (err)
 			throw err;
 		console.log(result);
@@ -27,11 +31,11 @@ exports.fetch = function(req, res) {
 	//console.log("Hey i reached here");
 }
 
-exports.statusUpdate = function(req, res) {
+exports.statusUpdate = function (req, res) {
 	console.log("order status update yahan to aa gya");
 	//res.write("hello");
 	var myobj = req.body;
-	order.update({"orderId":myobj.orderId},{$set: {status: myobj.status}},function(err, result) {
+	order.update({ "orderId": myobj.orderId }, { $set: { status: myobj.status } }, function (err, result) {
 		if (err)
 			throw err;
 		console.log(result);
@@ -40,11 +44,11 @@ exports.statusUpdate = function(req, res) {
 	console.log("Hey i reached here");
 }
 
-exports.addressUpdate = function(req, res) {
+exports.addressUpdate = function (req, res) {
 	console.log("order address update yahan to aa gya");
 	//res.write("hello");
 	var myobj = req.body;
-	order.update({"orderId":myobj.orderId},{$set: {address: myobj.address}},function(err, result) {
+	order.update({ "orderId": myobj.orderId }, { $set: { address: myobj.address } }, function (err, result) {
 		if (err)
 			throw err;
 		console.log(result);
