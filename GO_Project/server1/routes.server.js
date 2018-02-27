b54c71024d24c36f5420fc86a10c94231c931f8b
user <user@user-HP-Compaq-Elite-8300-SFF>
2018-02-27 10:16:44 +0530

    merge

--- GO_Project/server1/routes.server.js
+++ GO_Project/server1/routes.server.js
@@ -19,9 +19,9 @@ app.route('/address/search').get(address.search);
     app.route('/product/fetch').get(product.fetch);
     
     app.route('/cart/fetch').get(cart.fetch);
-    app.route('/cart/data').get(cart.fetch);
-    app.route('/cart/insert').get(cart.fetch);
-    app.route('/cart/delete').get(cart.fetch);
+    app.route('/cart/data').get(cart.data);
+    app.route('/cart/insert').get(cart.insert);
+    app.route('/cart/delete').get(cart.delete);
     
     app.route('/order/insert').get(order.insert);
     app.route('/order/fetch').get(order.fetch);
